cdd570557c419b3aa2d6021fdd5ccabbef3482b9
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-03-16 15:20:54 +0700

    chore: add cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.router import cbc, cfb, counter, ecb, ofb
 
-app = FastAPI()
+app = FastAPI(
+    title="API Tugas Besar 3 IF4020",
+    description="API Tugas Besar 3 IF4020",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cbc.router)
 app.include_router(cfb.router)
