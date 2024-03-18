e4e2e7bcd540e57465ba0af898d703245634aa0e
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-03-18 15:35:51 +0700

    refactor: change return to using JSONResponse

--- backend/router/ecb.py
+++ backend/router/ecb.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import APIRouter, Request, UploadFile, File
+from fastapi.responses import JSONResponse
 
 from backend.cipher.modes import ecb_decrypt, ecb_encrypt
 
@@ -9,37 +11,39 @@ router = APIRouter(
 )
 
 @router.post("/encrypt")
-async def encrypt(request: Request):
+async def encrypt(request: Request, file: Optional[UploadFile] = File(None)):
     try:
       # get request body
       body = await request.json()
       # get plaintext and key from request body
       plaintext, key, iv = body['plaintext'], body['key'], body['iv']
 
-      return {
-          "ciphertext": ecb_encrypt(plaintext, key, iv)
-      }
+      return JSONResponse(
+          content={"ciphertext": ecb_encrypt(plaintext, key, iv)},
+          status_code=200
+      )
 
     except Exception as e:
       # return error message
-      return {
-          "error": str(e)
-      }
+      return JSONResponse(
+          content={"error": str(e)},
+      )
 
 @router.post("/decrypt")
-async def decrypt(request: Request):
+async def decrypt(request: Request, file: Optional[UploadFile] = File(None)):
     try:
       # get request body
       body = await request.json()
       # get ciphertext and key from request body
       ciphertext, key, iv = body['ciphertext'], body['key'], body['iv']
 
-      return {
-        "ciphertext": ecb_decrypt(ciphertext, key, iv)
-      }
+      return JSONResponse(
+          content={"ciphertext": ecb_decrypt(ciphertext, key, iv)},
+          status_code=200
+      )
 
     except Exception as e:
       # return error message
-      return {
-          "error": str(e)
-      }
+      return JSONResponse(
+          content={"error": str(e)},
+      )
