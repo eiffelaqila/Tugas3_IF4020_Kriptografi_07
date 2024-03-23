db8a884dcbe7ecdc90821638233c7f11ee72fef5
eiffelaqila <13520074@std.stei.itb.ac.id>
2024-03-23 13:55:37 +0700

    remove unneeded files

--- backend/cipher/decryption.py
+++
@@ -1,4 +0,0 @@
-# Fungsi sederhana untuk dekripsi (sama dengan enkripsi karena ini contoh sederhana)
-def decrypt(ciphertext, key):
-    plaintext = bytes([b ^ key for b in ciphertext])
-    return plaintext
--- backend/cipher/encryption.py
+++
@@ -1,4 +0,0 @@
-# Fungsi sederhana untuk enkripsi (misalnya, XOR dengan kunci)
-def encrypt(plaintext, key):
-    ciphertext = bytes([b ^ key for b in plaintext])
-    return ciphertext
